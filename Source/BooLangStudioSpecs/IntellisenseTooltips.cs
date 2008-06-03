fe77defc5fc4f6a2f01478424245f87d8112cb61
James Gregory <james@jagregory.com>
2008-06-03 19:37:18 +0100

    * Started adding tests, finally. Two separate groups, one for intellisense parsing of the document, and the other for the descriptions that are displayed from the intellisense. * Added Interfaces into the intellisense parse.

---
+++ Source/BooLangStudioSpecs/IntellisenseTooltips.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Boo.BooLangService;
+using Boo.BooLangService.Document.Nodes;
+using MbUnit.Framework;
+using Context = MbUnit.Framework.TestFixtureAttribute;
+using Spec = MbUnit.Framework.TestAttribute;
+
+namespace Boo.BooLangStudioSpecs
+{
+    [Context]
+    public class WhenDisplayingDescriptionsForIntellisense
+    {
+        [Spec]
+        public void ClassesArePrefixedWithClassAndUseFullNamespaceName()
+        {
+            var declarations = CreateDeclarations(new ClassTreeNode {Name = "MyClass", FullName = "Some.Namespace.MyClass"});
+
+            string description = declarations.GetDescription(0);
+
+            Assert.IsTrue(description == "Class Some.Namespace.MyClass",
+                "Expected: 'Class Some.Namespace.MyClass'\r\nFound: '" + description + "'.");
+        }
+
+        [Spec]
+        public void FieldsStartWithTypeFollowedByDeclaredName()
+        {
+            var declarations = CreateDeclarations(new LocalTreeNode { Name = "variable", ReturnType = "int" });
+
+            string description = declarations.GetDescription(0);
+
+            Assert.IsTrue(description == "int variable",
+                "Expected: 'int variable'\r\nFound: '" + description + "'.");
+        }
+
+        private BooDeclarations CreateDeclarations(params IBooParseTreeNode[] nodes)
+        {
+            var declarations = new BooDeclarations();
+
+            declarations.Add(new List<IBooParseTreeNode>(nodes));
+
+            return declarations;
+        }
+    }
+}
