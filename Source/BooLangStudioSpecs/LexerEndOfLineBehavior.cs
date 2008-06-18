c5de37dcf85a95a475b61586ec73c8b0a3b75c7c
olsonjeffery <olson.jeffery@gmail.com>
2008-06-17 17:38:38 -0700

    stubs out a fixture for specify the lexer's desired behavior when it parses to the end of a line of code
    Signed-off-by: olsonjeffery <olson.jeffery@gmail.com>

---
+++ Source/BooLangStudioSpecs/LexerEndOfLineBehavior.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using Microsoft.VisualStudio.Package;
+using BooLangService;
+using BooPegLexer;
+using Rhino.Mocks;
+using Boo.Pegs;
+
+namespace Boo.BooLangStudioSpecs
+{
+	/// <summary>
+	/// Description of LexerEndOfLineBehavior.
+	/// </summary>
+	public class WhenParsingTheLastTokenInALine : LexerPartialMockFixture
+	{
+		protected int state = 0;
+		protected PegToken pegToken = new PegToken();
+		protected string line = string.Empty;
+		public WhenParsingTheLastTokenInALine()
+			: base()
+		{
+			
+		}
+	}
+}
