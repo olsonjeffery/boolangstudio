aec33fe99a42f0f27e3eebb9927c81a088f5a346
Jeffery Olson <olson.jeffery@gmail.com>
2008-09-13 08:43:58 -0700

    squared away whitespace in BooProjectNode.cs .. also added this.CanProjectDeleteItems = true .. make sure we can delete items. added new spec to the tests to verify the above change... this is a regression issue (was in past versions, disapeared in merges)

--- Source/BooLangProject/BooProjectNode.cs
+++ Source/BooLangProject/BooProjectNode.cs
@@ -12,198 +12,200 @@ using System.Reflection;
 
 namespace Boo.BooLangProject
 {
-    [CLSCompliant(false)]
-    [ComVisible(true)]
-    [Guid(GuidList.guidBooProjectClassString)]
-    public class BooProjectNode : ProjectNode
+  [CLSCompliant(false)]
+  [ComVisible(true)]
+  [Guid(GuidList.guidBooProjectClassString)]
+  public class BooProjectNode : ProjectNode
+  {
+    private static ImageList imageList;
+    internal static int booFileNodeImageIndex;
+    internal static int imageIndex;
+
+    private ProjectPackage package;
+    private BooVSProject vsProject;
+    private BooProjectSources projectSources;
+    private readonly BooLangService.BooLangService languageService;
+
+    static BooProjectNode()
     {
-        private static ImageList imageList;
-        internal static int booFileNodeImageIndex;
-        internal static int imageIndex;
+      imageList =
+          Utilities.GetImageList(
+          typeof(BooProjectNode).Assembly.GetManifestResourceStream(
+          "Boo.BooLangProject.Resources.BooProjectNode.bmp"));
 
-        private ProjectPackage package;
-        private BooVSProject vsProject;
-        private BooProjectSources projectSources;
-        private readonly BooLangService.BooLangService languageService;
+      ImageList booFileNodeImageList;
+      string booFileResourceString = "Boo.BooLangProject.Resources.BooFileNode.bmp";
+      try
+      {
 
-        static BooProjectNode()
-        {
-            imageList =
-                Utilities.GetImageList(
-                typeof(BooProjectNode).Assembly.GetManifestResourceStream(
-                "Boo.BooLangProject.Resources.BooProjectNode.bmp"));
+        booFileNodeImageList =
+            Utilities.GetImageList(
+            typeof(BooProjectNode).Assembly.GetManifestResourceStream(
+            booFileResourceString));
+      }
+      catch (Exception e)
+      {
+        throw;
+      }
 
-            ImageList booFileNodeImageList;
-            string booFileResourceString = "Boo.BooLangProject.Resources.BooFileNode.bmp";
-            try
-            {
+      if (booFileNodeImageList.Images.Count != 1)
+        throw new FileNotFoundException("Cannot find Boo FileNode Icon at: " + booFileResourceString);
+      else
+        imageList.Images.Add(booFileNodeImageList.Images[0]);
 
-                booFileNodeImageList =
-                    Utilities.GetImageList(
-                    typeof(BooProjectNode).Assembly.GetManifestResourceStream(
-                    booFileResourceString));
-            }
-            catch (Exception e)
-            {
-                throw;
-            }
-
-            if (booFileNodeImageList.Images.Count != 1)
-                throw new FileNotFoundException("Cannot find Boo FileNode Icon at: " + booFileResourceString);
-            else
-                imageList.Images.Add(booFileNodeImageList.Images[0]);
-
-        }
-
-        public BooProjectNode(ProjectPackage package, BooLangService.BooLangService languageService)
-        {
-            this.package = package;
-            this.languageService = languageService;
-            imageIndex = this.ImageHandler.ImageList.Images.Count;
-            booFileNodeImageIndex = imageIndex + 1;
-
-            foreach (Image img in imageList.Images)
-            {
-                this.ImageHandler.AddImage(img);
-            }
-        }
-
-        internal override object Object
-        {
-            get
-            {
-                if (vsProject == null)
-                    vsProject = new BooVSProject(this);
-
-                return vsProject;
-            }
-        }
-
-        /// <summary>
-        /// This is a very poor workaround. Until I can figure out how
-        /// to get our GlobalProperies into the ProjectShim that the
-        /// base version uses...
-        /// </summary>
-        /// <returns></returns>
-        protected override ProjectLoadOption IsProjectSecure()
-        {
-            return ProjectLoadOption.LoadNormally;
-        }
-
-        public override Guid ProjectGuid
-        {
-            get { return typeof(BooLangProjectFactory).GUID; }
-        }
-
-        public override string ProjectType
-        {
-            get 
-            { 
-                //return this.GetType().Name;
-                return "BooProjectType";
-            }
-        }
-
-        public override void AddFileFromTemplate(string source, string target)
-        {
-            string ns = this.FileTemplateProcessor.GetFileNamespace(target, this);
-            this.FileTemplateProcessor.AddReplace("$nameSpace$", ns);
-            this.FileTemplateProcessor.UntokenFile(source, target);
-            this.FileTemplateProcessor.Reset();
-        }
-
-        public override FileNode CreateFileNode(ProjectElement item)
-        {
-            return new BooFileNode(this, item, booFileNodeImageIndex) as FileNode;
-        }
-
-        /// <summary>
-        /// If you don't override this, then all new files you add to your
-        /// project will be of type "content" by default and not "compile".
-        /// You'll also get AIDS.
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <returns></returns>
-        public override bool IsCodeFile(string fileName)
-        {
-            if (new FileInfo(fileName).Extension.ToLower().Contains("boo"))
-                return true;
-            else
-                return false;
-        }
-
-        /// <summary>
-        /// Overriding to provide project general property page
-        /// </summary>
-        /// <returns></returns>
-        protected override Guid[] GetConfigurationIndependentPropertyPages()
-        {
-          Guid[] result = new Guid[1];
-          result[0] = typeof(GeneralPropertyPage).GUID;
-          return result;
-        }
-
-        /// <summary>
-        /// provides the guid for configuration dependent settings
-        /// </summary>
-        /// <param name="config"></param>
-        /// <returns></returns>
-        protected override Guid[] GetConfigurationDependentPropertyPages()
-        {
-            Guid[] result = new Guid[1];
-            result[0] = typeof(BooBuildPropertyPage).GUID;
-            return result;
-        }
-
-        public override int ImageIndex
-        {
-
-            get { return imageIndex + 0; }
-
-        }
-
-        public override object GetAutomationObject()
-        {
-            return new BooOAProject(this);
-        }
-
-        public override void Load(string fileName, string location, string name, uint flags, ref Guid iidProject, out int canceled)
-        {
-            // this needs to be instantiated before the base call, and then start watching
-            // the hierarchy after. This is because the base.Load call hits the AddChild
-            // method of the references, which add to the project sources. The watching
-            // needs to start after the base call because if it's started before, then there
-            // aren't any files added yet!
-            projectSources = new BooProjectSources(languageService);
-
-            base.Load(fileName, location, name, flags, ref iidProject, out canceled);
-
-            projectSources.StartWatchingHierarchy(InteropSafeHierarchy);
-
-            BooProjectSources.LoadedProjects.Add(projectSources);
-        }
-
-        IVsHierarchy InteropSafeHierarchy
-        {
-            get
-            {
-                IntPtr unknownPtr = Utilities.QueryInterfaceIUnknown(this);
-
-                if (unknownPtr == IntPtr.Zero)
-                    return null;
-
-                return (IVsHierarchy)Marshal.GetObjectForIUnknown(unknownPtr);
-            }
-        }
-
-        public BooProjectSources Sources
-        {
-            get { return projectSources; }
-        }
-
-        protected override ReferenceContainerNode CreateReferenceContainerNode()
-        {
-            return new BooReferenceContainerNode(this);
-        }
     }
+
+    public BooProjectNode(ProjectPackage package, BooLangService.BooLangService languageService)
+    {
+      this.package = package;
+      this.languageService = languageService;
+      imageIndex = this.ImageHandler.ImageList.Images.Count;
+      booFileNodeImageIndex = imageIndex + 1;
+
+      this.CanProjectDeleteItems = true;
+
+      foreach (Image img in imageList.Images)
+      {
+        this.ImageHandler.AddImage(img);
+      }
+    }
+
+    internal override object Object
+    {
+      get
+      {
+        if (vsProject == null)
+          vsProject = new BooVSProject(this);
+
+        return vsProject;
+      }
+    }
+
+    /// <summary>
+    /// This is a very poor workaround. Until I can figure out how
+    /// to get our GlobalProperies into the ProjectShim that the
+    /// base version uses...
+    /// </summary>
+    /// <returns></returns>
+    protected override ProjectLoadOption IsProjectSecure()
+    {
+      return ProjectLoadOption.LoadNormally;
+    }
+
+    public override Guid ProjectGuid
+    {
+      get { return typeof(BooLangProjectFactory).GUID; }
+    }
+
+    public override string ProjectType
+    {
+      get
+      {
+        //return this.GetType().Name;
+        return "BooProjectType";
+      }
+    }
+
+    public override void AddFileFromTemplate(string source, string target)
+    {
+      string ns = this.FileTemplateProcessor.GetFileNamespace(target, this);
+      this.FileTemplateProcessor.AddReplace("$nameSpace$", ns);
+      this.FileTemplateProcessor.UntokenFile(source, target);
+      this.FileTemplateProcessor.Reset();
+    }
+
+    public override FileNode CreateFileNode(ProjectElement item)
+    {
+      return new BooFileNode(this, item, booFileNodeImageIndex) as FileNode;
+    }
+
+    /// <summary>
+    /// If you don't override this, then all new files you add to your
+    /// project will be of type "content" by default and not "compile".
+    /// You'll also get AIDS.
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <returns></returns>
+    public override bool IsCodeFile(string fileName)
+    {
+      if (new FileInfo(fileName).Extension.ToLower().Contains("boo"))
+        return true;
+      else
+        return false;
+    }
+
+    /// <summary>
+    /// Overriding to provide project general property page
+    /// </summary>
+    /// <returns></returns>
+    protected override Guid[] GetConfigurationIndependentPropertyPages()
+    {
+      Guid[] result = new Guid[1];
+      result[0] = typeof(GeneralPropertyPage).GUID;
+      return result;
+    }
+
+    /// <summary>
+    /// provides the guid for configuration dependent settings
+    /// </summary>
+    /// <param name="config"></param>
+    /// <returns></returns>
+    protected override Guid[] GetConfigurationDependentPropertyPages()
+    {
+      Guid[] result = new Guid[1];
+      result[0] = typeof(BooBuildPropertyPage).GUID;
+      return result;
+    }
+
+    public override int ImageIndex
+    {
+
+      get { return imageIndex + 0; }
+
+    }
+
+    public override object GetAutomationObject()
+    {
+      return new BooOAProject(this);
+    }
+
+    public override void Load(string fileName, string location, string name, uint flags, ref Guid iidProject, out int canceled)
+    {
+      // this needs to be instantiated before the base call, and then start watching
+      // the hierarchy after. This is because the base.Load call hits the AddChild
+      // method of the references, which add to the project sources. The watching
+      // needs to start after the base call because if it's started before, then there
+      // aren't any files added yet!
+      projectSources = new BooProjectSources(languageService);
+
+      base.Load(fileName, location, name, flags, ref iidProject, out canceled);
+
+      projectSources.StartWatchingHierarchy(InteropSafeHierarchy);
+
+      BooProjectSources.LoadedProjects.Add(projectSources);
+    }
+
+    IVsHierarchy InteropSafeHierarchy
+    {
+      get
+      {
+        IntPtr unknownPtr = Utilities.QueryInterfaceIUnknown(this);
+
+        if (unknownPtr == IntPtr.Zero)
+          return null;
+
+        return (IVsHierarchy)Marshal.GetObjectForIUnknown(unknownPtr);
+      }
+    }
+
+    public BooProjectSources Sources
+    {
+      get { return projectSources; }
+    }
+
+    protected override ReferenceContainerNode CreateReferenceContainerNode()
+    {
+      return new BooReferenceContainerNode(this);
+    }
+  }
 }
---
+++ Source/BooLangStudioSpecs/BooProject/DeletingFilesFromTheProject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Boo.BooLangProject;
+using Boo.BooLangStudio;
+using Boo.BooLangService;
+using Xunit;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Boo.BooLangStudioSpecs
+{
+  public class when_creating_a_new_BooProjectNode_object
+  {
+    protected BooProjectNode node;
+    public when_creating_a_new_BooProjectNode_object()
+    {
+      var package = new BooLangStudioPackage();
+      var service = new Boo.BooLangService.BooLangService();
+      node = new BooProjectNode(package, service);
+    }
+
+    [Fact]
+    public void should_have_its_CanProjectDeleteItems_set_to_true()
+    {
+      Assert.True(node.CanProjectDeleteItems);
+    }
+  }
+}
